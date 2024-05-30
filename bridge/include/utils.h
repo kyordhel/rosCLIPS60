0674eb4469e48451f7dbd4beb62761d6252de58e
Mauricio Matamoros <kyordhel@gmail.com>
2024-05-30 12:54:07 -0600

    Add documentation for bridge/include/utils.h

--- bridge/include/utils.h
+++ bridge/include/utils.h
@@ -1,13 +1,44 @@
+/* ** ***************************************************************
+* utils.h
+*
+* Author: Mauricio Matamoros
+*
+* Utility/miscellaneous/helper functions for the bridge
+*
+** ** **************************************************************/
 #ifndef __UTILS_H__
 #define __UTILS_H__
+#pragma once
 
 #include <string>
 
 namespace utils{
 
-	int32_t xtractInt(std::string const& s);
-	uint32_t xtractUint(std::string const& s);
-	double xtractDouble(std::string const& s);
+/**
+ * Retrieves the first integer found in the given string if any.
+ * If no integer is found returns zero.
+ * @param  s The string containing an integer
+ * @return   The first int32 found in the given string, zero if none.
+ */
+int32_t xtractInt(std::string const& s);
+
+/**
+ * Retrieves the first unsigned integer found in the given string,
+ * if any. If no integer is found returns zero.
+ * @param  s The string containing an unsigned integer
+ * @return   The first uint32 found in the given string, zero if none.
+ */
+uint32_t xtractUint(std::string const& s);
+
+/**
+ * Retrieves the first decimal number found in the given string,
+ * if any as a double-precision float. If no integer is found returns
+ * zero.
+ *
+ * @param  s The string containing a decimal number
+ * @return   The first number found in the given string, zero if none.
+ */
+double xtractDouble(std::string const& s);
 
 } // end namespace
 
